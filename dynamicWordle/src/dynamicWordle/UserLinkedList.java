f6e60d08b41693ff450eef977d2059b50b5d81ec
Nin-dev <kaleb.stem@gmail.com>
2024-06-17 09:04:43 -0600

    UserLinkedList

--- dynamicWordle/src/dynamicWordle/UserLinkedList.java
+++ dynamicWordle/src/dynamicWordle/UserLinkedList.java
@@ -1,5 +1,15 @@
 package dynamicWordle;
 
 public class UserLinkedList {
-
+	
+	
+	public class Node {
+		User user;
+		Node next;
+		
+		public Node(User user) {
+			this.user = user;
+			next = null;
+		}
+	}
 }
