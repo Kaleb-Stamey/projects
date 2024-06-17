95c948cdbdeee9119ba2d209abd20fb928263f12
Nin-dev <kaleb.stem@gmail.com>
2024-06-16 21:30:31 -0600

    UserLinkedList

--- dynamicWordle/src/dynamicWordle/dynamicWordle.java
+++ dynamicWordle/src/dynamicWordle/dynamicWordle.java
@@ -5,7 +5,10 @@ import java.util.LinkedList;
 public class dynamicWordle {
 		public static void main(String[] args) {
 			LinkedList<String> myList = new LinkedList<>();
+			
 			Game myGame = new Game(myList);
 			myGame.playGame();
+			
+			
 		}		
 }
